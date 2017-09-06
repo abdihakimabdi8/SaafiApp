f31c0fb6dc14118ba574ebb76f105e77fef925a9
Hakim Abdi <Hakim.Abdi@VML.com>
2017-09-05 21:05:11 -0500

    IMainViewModel edtied

--- SaafiApp/SaafiApp.Core/Contracts/ViewModel/IMainViewModel.cs
+++ SaafiApp/SaafiApp.Core/Contracts/ViewModel/IMainViewModel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SaafiApp.Core.Contracts.Services
+namespace SaafiApp.Core.Contracts.ViewModel
 {
     public interface IMainViewModel
     {
